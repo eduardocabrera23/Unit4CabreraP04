541795104f53a8fb264bdf4a8d66014f44925e34
eduardocabrera23 <768550@student.dusd.net>
2026-04-29 22:40:55 -0700

    need help with smashing powerup

--- P04CabreraUnit/Assets/Scripts/PlayerController.cs
+++ P04CabreraUnit/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,13 @@ public class PlayerController : MonoBehaviour
     public GameObject rocketPrefab;
     private GameObject tmpRocket;
     private Coroutine powerupCountdown;
+
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+    bool smashing = false;
+    float floorY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -34,8 +41,46 @@ public class PlayerController : MonoBehaviour
             LaunchRockets();
 
         }
+        if(currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && 
+        !smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
+        }
 
     }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+        //Store the y position before taking off
+        floorY = transform.position.y;
+        //Calculate the amount of time we will go up
+        float jumpTime = Time.time + hangTime;
+        while (Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity.
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+        //Now move the player down
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+        //Cycle through all enemies.
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            //Apply an explosion force that originates from our position.
+            if (enemies[i] != null)
+                enemies[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce,
+                transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+        }
+        //We are no longer smashing, so set the boolean to false
+        smashing = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Powerup"))
