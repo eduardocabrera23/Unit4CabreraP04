8ac684da3f077bd8ff15b650c54417cc88be6a0d
eduardocabrera23 <768550@student.dusd.net>
2026-05-03 23:00:14 -0700

    finished challenge

--- P04CabreraUnit/Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ P04CabreraUnit/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -13,7 +13,7 @@ public class SpawnManagerX : MonoBehaviour
 
     public int enemyCount;
     public int waveCount = 1;
-
+    public float enemySpeed = 50;
 
     public GameObject player; 
 
@@ -56,6 +56,7 @@ public class SpawnManagerX : MonoBehaviour
 
         waveCount++;
         ResetPlayerPosition(); // put player back at start
+        enemySpeed += 25;
 
     }
 
